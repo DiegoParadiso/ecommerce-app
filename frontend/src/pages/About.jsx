bef9c760c04c0fa01fd6020c7175497dbcd34246
Diego Gonzalez <diego.gonzaaleez.a@gmail.com>
2025-04-09 15:58:39 -0300

    Se crea sección About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Title from '../components/Title'
+import React from 'react';
+import Title from '../components/Title';
 
 const About = () => {
   return (
-    <div className='my-8'>
-      <div className='text-center'>
-        <Title text1={'ACERCA DE'} text2={'NOSOTROS'}> </Title>
+    <div className='my-8 px-4 max-w-3xl mx-auto text-center text-gray-700'>
+      <div className='mb-10'>
+        <Title text1={'ACERCA DE'} text2={'NOSOTROS'} />
+      </div>
+
+      <div className='space-y-16 leading-relaxed text-sm'>
+        <p className='leading-loose'>
+        Nos especializamos en la pesca de prendas únicas en tiendas vintage, mercados de pulgas y tiendas de segunda mano de todo el país que llegan de cualquier parte del mundo, rescatando piezas con historia, carácter y autenticidad.
+        </p>
+        <p className='leading-loose'>
+        Cada artículo es cuidadosamente seleccionado para garantizar su calidad, asi como también su curado e intervención si es necesario.
+        </p>
+        <p className='leading-loose'>
+        Nos esforzamos por crear una experiencia de compra única y personalizada. Nuestro equipo está apasionado por esta corriente y está siempre dispuesto a asesorarte para que encuentres las prendas que mejor se adapten a tu personalidad y estilo.
+        </p>
+        <p className='leading-loose'>
+          Gracias por formar parte de esta comunidad.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
